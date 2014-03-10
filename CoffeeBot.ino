41a45e2c6f7bb6967e8d1cf811205920421329ab
Joel Geiger <joel@alltooeasy.com>
2014-03-09 21:20:18 -0400

    Change the pin assignments for how I actually wired it, and light the LEDs.

--- CoffeeBot.ino
+++ CoffeeBot.ino
@@ -4,16 +4,20 @@ Try this program first, and then start modifying it to give your robot the perso
 Shine a flashlight, or hold your hand over a light sensor, to change the behaviour.
 */
 
-const int leftMotorPin = 3;
-const int rightMotorPin = 5;
+const int leftMotorPin = 5;
+const int rightMotorPin = 3;
 const int leftLightSensor = A0;
 const int rightLightSensor = A2;
+const int leftLed = 11;
+const int rightLed = 9;
 const int led = 13;
 
 void setup()
 {
   pinMode( leftMotorPin, OUTPUT );
   pinMode( rightMotorPin, OUTPUT );
+  pinMode( leftLed, OUTPUT );
+  pinMode( rightLed, OUTPUT );
   pinMode( led, OUTPUT );
   
   pinMode( leftLightSensor, INPUT );
@@ -33,23 +37,29 @@ void loop()
   
   delay( 2000 );
 */
-  if ( analogRead( leftLightSensor ) > analogRead( rightLightSensor ) )
+  if ( analogRead( leftLightSensor ) < analogRead( rightLightSensor ) )
   {
     digitalWrite( led, HIGH );
     digitalWrite( leftMotorPin, HIGH );
     digitalWrite( rightMotorPin, LOW );
+    digitalWrite( leftLed, LOW );
+    digitalWrite( rightLed, HIGH );
   }
-  else if ( analogRead( leftLightSensor ) < analogRead( rightLightSensor ) )
+  else if ( analogRead( leftLightSensor ) > analogRead( rightLightSensor ) )
   {
     digitalWrite( led, LOW );  
     digitalWrite( leftMotorPin, LOW );
     digitalWrite( rightMotorPin, HIGH );
+    digitalWrite( leftLed, HIGH );
+    digitalWrite( rightLed, LOW );
   }
   else
   {
     digitalWrite( led, LOW );  
     digitalWrite( leftMotorPin, HIGH );
     digitalWrite( rightMotorPin, HIGH );
+    digitalWrite( leftLed, HIGH );
+    digitalWrite( rightLed, HIGH );
   }
   
 }
